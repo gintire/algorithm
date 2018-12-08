3a57317510b065381aa43a94e6655218191d277e
Gintire <jin3670@gmail.com>
2018-12-09 01:24:26 +0900

    Fibonacci ADD README.md

---
+++ algorithm/fibonacci/README.md
@@ -0,0 +1,5 @@
+Fibonacci
+=========
+_Describe detail in my Blog below_
+
+Goto [GintireBlog](https://gintrie.tistory.com/2)
--- algorithm/fibonacci/src/test/java/fibonacci/FibonacciTest.java
+++ algorithm/fibonacci/src/test/java/fibonacci/FibonacciTest.java
@@ -8,9 +8,8 @@ public class FibonacciTest {
 
 	@Test
 	public void testFibo() {
-		int cases[][] = {{0,0}, {1,1}, {2,1}, {3,2}};
+		int cases[][] = {{0,0}, {1,1}, {2,1}, {3,2}, {4,3}, {5,5}}; 
 		for(int i=0; i<cases.length;i++)
 			assertEquals(cases[i][1], Fibonacci.fib(cases[i][0]));
 	}
-
 }

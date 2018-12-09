70557bedc32091a481e348f2b4a1721669c08fa4
Gintire <jin3670@gmail.com>
2018-12-10 01:32:06 +0900

    회전하는 면의 색상도 회전한다는것을 캐치하지 못함. 수정전

--- algorithm/Cubing/src/main/java/Main.java
+++ algorithm/Cubing/src/main/java/Main.java
@@ -30,22 +30,295 @@ public class Main {
 	public static String solve1(String string) {
 		initCube();
 		String oriStr = string;
-		String[] str = oriStr.split("");
 		String resStr = "";
-		for(int j=0; j<str.length; j+=2) {
-			if(str[j].equals("L")) {
-				if(str[j+1].equals("+")) turnU(true);
-				if(str[j+1].equals("-")) turnU(false);
-			}
-			for(int a=0; a<3; a++) {
-				for (int b=0; b<3; b++) {
-					resStr+=cube[0][a][b];
+		String[] str = oriStr.split(" ");
+		for(int i=0; i<str.length; i++) {
+			String[] str1 = str[i].split("");
+			for(int j=0; j<str1.length; j+=2) {
+				if(str1[j].equals("U")) {
+					if(str1[j+1].equals("+")) turnU(true);
+					if(str1[j+1].equals("-")) turnU(false);
 				}
+				if(str1[j].equals("D")) {
+					if(str1[j+1].equals("+")) turnD(true);
+					if(str1[j+1].equals("-")) turnD(false);
+				}
+				if(str1[j].equals("F")) {
+					if(str1[j+1].equals("+")) turnF(true);
+					if(str1[j+1].equals("-")) turnF(false);
+				}
+				if(str1[j].equals("B")) {
+					if(str1[j+1].equals("+")) turnB(true);
+					if(str1[j+1].equals("-")) turnB(false);
+				}
+				if(str1[j].equals("L")) {
+					
+					if(str1[j+1].equals("+")) turnL(true);
+					if(str1[j+1].equals("-")) turnL(false);
+				}
+				if(str1[j].equals("R")) {
+					if(str1[j+1].equals("+")) turnR(true);
+					if(str1[j+1].equals("-")) turnR(false);
+				}
+			}
+			
+		}
+		for(int a=0; a<3; a++) {
+			for (int b=0; b<3; b++) {
+				resStr+=cube[0][a][b];
 			}
 		}
 		return resStr;
 	}
 	private static void turnU (Boolean flag) {
+		// '+' 시계방향
+		if(flag) {
+			String temp[] = new String[9];
+			//cube[2][0][0] -> cube[4][0][2]
+			//cube[2][0][1] -> cube[4][1][2]
+			//cube[2][0][2] -> cube[4][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[4][i][2];
+				cube[4][i][2] = cube[2][0][i];
+			}
+			//cube[4][0][2] -> cube[3][2][2]
+			//cube[4][1][2] -> cube[3][2][1]
+			//cube[4][2][2] -> cube[3][2][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3] = cube[3][2][2-i];
+				cube[3][2][2-i] = temp[i];
+			}
+			//cube[3][2][2] -> cube[5][2][0]
+			//cube[3][2][1] -> cube[5][1][0]
+			//cube[3][2][0] -> cube[5][0][0]
+			for(int i=0; i<3; i++) {
+				temp[i+6] = cube[5][2-i][0];
+				cube[5][2-i][0] = temp[i+3];
+			}
+			//cube[5][2][0] -> cube[2][0][0]
+			//cube[5][1][0] -> cube[2][0][1]
+			//cube[5][0][0] -> cube[2][0][2]
+			for(int i=0; i<3; i++) {
+				cube[2][0][i] = temp[i+6];
+			}
+		} else {
+			String temp[] = new String[9];
+			//cube[2][0][0] -> cube[5][2][0]
+			//cube[2][0][1] -> cube[5][1][0]
+			//cube[2][0][2] -> cube[5][0][0]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[5][2-i][0];
+				cube[5][2-i][0] = cube[2][0][i];
+			}
+			//cube[5][2][0] -> cube[3][2][2]
+			//cube[5][1][0] -> cube[3][2][1]
+			//cube[5][0][0] -> cube[3][2][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3] = cube[3][2][2-i];
+				cube[3][2][2-i] = temp[i];
+			}
+			//cube[3][2][2] -> cube[4][0][2]
+			//cube[3][2][1] -> cube[4][1][2]
+			//cube[3][2][0] -> cube[4][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6] = cube[4][i][2];
+				cube[4][i][2] = temp[i+3];
+			}
+			//cube[4][0][2] -> cube[2][0][0]
+			//cube[4][1][2] -> cube[2][0][1]
+			//cube[4][2][2] -> cube[2][0][2]
+			for(int i=0; i<3; i++) {
+				cube[2][0][i] = temp[i+6];
+			}
+		}
+	}
+	private static void turnD (Boolean flag) {
+		// '+' 시계방향
+		if(flag) {
+			String temp[] = new String[9];
+			//cube[2][2][0] -> cube[5][2][2]
+			//cube[2][2][1] -> cube[5][1][2]
+			//cube[2][2][2] -> cube[5][0][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[5][2-i][2];
+				cube[5][2-i][2] = cube[2][2][i];
+			}
+			//cube[5][2][2] -> cube[3][0][2]
+			//cube[5][1][2] -> cube[3][0][1]
+			//cube[5][0][2] -> cube[3][0][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3] = cube[3][0][2-i];
+				cube[3][2][i] = temp[i];
+			}
+			//cube[3][0][2] -> cube[4][0][0]
+			//cube[3][0][1] -> cube[4][1][0]
+			//cube[3][0][0] -> cube[4][2][0]
+			for(int i=0; i<3; i++) {
+				temp[i+6] = cube[4][i][0];
+				cube[4][i][0] = temp[i+3];
+			}
+			//cube[4][0][0] -> cube[2][2][0]
+			//cube[4][1][0] -> cube[2][2][1]
+			//cube[4][2][0] -> cube[2][2][2]
+			for(int i=0; i<3; i++) {
+				cube[2][2][i] = temp[i+6];
+			}
+		} else {
+			String temp[] = new String[9];
+			//cube[2][2][0] -> cube[4][0][0]
+			//cube[2][2][1] -> cube[4][1][0]
+			//cube[2][2][2] -> cube[4][2][0]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[4][i][0];
+				cube[4][i][0] = cube[2][2][i];
+			}
+			//cube[4][0][0] -> cube[3][0][2]
+			//cube[4][1][0] -> cube[3][0][1]
+			//cube[4][2][0] -> cube[3][0][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3] = cube[3][0][2-i];
+				cube[3][2][2-i] = temp[i];
+			}
+			//cube[3][0][2] -> cube[5][2][2]
+			//cube[3][0][1] -> cube[5][1][2]
+			//cube[3][0][0] -> cube[5][0][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6] = cube[5][2-i][2];
+				cube[5][2-i][2] = temp[i+3];
+			}
+			//cube[5][2][2] -> cube[2][2][0]
+			//cube[5][1][2] -> cube[2][2][1]
+			//cube[5][0][2] -> cube[2][2][2]
+			for(int i=0; i<3; i++) {
+				cube[2][2][i] = temp[i+6];
+			}
+		} 
+	}
+	private static void turnF (Boolean flag) {
+		// '+' 시계방향
+		if(flag) {
+			String temp[] = new String[9];
+			//cube[0][2][0] -> cube[5][2][0]
+			//cube[0][2][1] -> cube[5][2][1]
+			//cube[0][2][2] -> cube[5][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[5][2][i];
+				cube[5][2][i] = cube[0][2][i];
+			}
+			//cube[5][2][0] -> cube[1][0][2]
+			//cube[5][2][1] -> cube[1][0][1]
+			//cube[5][2][2] -> cube[1][0][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3]=cube[1][0][2-i];
+				cube[1][0][2-i] = temp[i];
+			}
+			//cube[1][0][2] -> cube[4][2][0]
+			//cube[1][0][1] -> cube[4][2][1]
+			//cube[1][0][0] -> cube[4][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6]=cube[4][2][i];
+				cube[1][0][2-i] = temp[i+3];
+			}
+			//cube[4][2][0] -> cube[0][2][0]
+			//cube[4][2][1] -> cube[0][2][1]
+			//cube[4][2][2] -> cube[0][2][2]
+			for(int i=0; i<3; i++) {
+				cube[0][2][i] = temp[i+6];
+			}
+		} else {
+			String temp[] = new String[9];
+			//cube[0][2][0] -> cube[4][2][0]
+			//cube[0][2][1] -> cube[4][2][1]
+			//cube[0][2][2] -> cube[4][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[4][2][i];
+				cube[4][2][i] = cube[0][2][i];
+			}
+			//cube[4][2][0] -> cube[1][0][2]
+			//cube[4][2][1] -> cube[1][0][1]
+			//cube[4][2][2] -> cube[1][0][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3]=cube[1][0][2-i];
+				cube[1][0][2-i] = temp[i];
+			}
+			//cube[1][0][0] -> cube[5][2][0]
+			//cube[1][0][1] -> cube[5][2][1]
+			//cube[1][0][2] -> cube[5][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6]=cube[5][2][i];
+				cube[5][2][i] = temp[i+3];
+			}
+			//cube[5][2][0] -> cube[0][2][0]
+			//cube[5][2][1] -> cube[0][2][1]
+			//cube[5][2][2] -> cube[0][2][2]
+			for(int i=0; i<3; i++) {
+				cube[0][2][i] = temp[i+6];
+			}
+		}
+	}
+	private static void turnB (Boolean flag) {
+		// '+' 시계방향
+		if(flag) {
+			String temp[] = new String[9];
+			//cube[0][0][0] -> cube[4][0][0]
+			//cube[0][0][1] -> cube[4][0][1]
+			//cube[0][0][2] -> cube[4][0][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[4][0][i];
+				cube[4][0][i] = cube[0][0][i];
+			}
+			//cube[4][0][0] -> cube[1][2][2]
+			//cube[4][0][1] -> cube[1][2][1]
+			//cube[4][0][2] -> cube[1][2][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3]=cube[1][2][2-i];
+				cube[1][2][2-i] = temp[i];
+			}
+			//cube[1][2][2] -> cube[5][0][0]
+			//cube[1][2][1] -> cube[5][0][1]
+			//cube[1][2][0] -> cube[5][0][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6]=cube[5][0][i];
+				cube[5][0][i] = temp[i+3];
+			}
+			//cube[5][0][0] -> cube[0][0][0]
+			//cube[5][0][1] -> cube[0][0][1]
+			//cube[5][0][2] -> cube[0][0][2]
+			for(int i=0; i<3; i++) {
+				cube[0][0][i] = temp[i+6];
+			}
+		} else {
+			String temp[] = new String[9];
+			//cube[0][0][0] -> cube[5][0][0]
+			//cube[0][1][0] -> cube[5][0][1]
+			//cube[0][2][0] -> cube[5][0][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[5][0][i];
+				cube[5][0][i] = cube[0][i][0];
+			}
+			//cube[5][0][0] -> cube[1][2][2]
+			//cube[5][0][1] -> cube[1][2][1]
+			//cube[5][0][2] -> cube[1][2][0]
+			for(int i=0; i<3; i++) {
+				temp[i+3]=cube[1][2][2-i];
+				cube[1][2][2-i] = temp[i];
+			}
+			//cube[1][2][2] -> cube[4][0][0]
+			//cube[1][2][1] -> cube[4][0][1]
+			//cube[1][2][0] -> cube[4][0][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6]=cube[4][0][i];
+				cube[4][0][i] = temp[i+3];
+			}
+			//cube[4][0][0] -> cube[0][0][0]
+			//cube[4][0][1] -> cube[0][0][1]
+			//cube[4][0][2] -> cube[0][0][2]
+			for(int i=0; i<3; i++) {
+				cube[0][0][i] = temp[i+6];
+			}
+		}
+	}
+	private static void turnL (Boolean flag) {
 		// '+' 시계방향
 		if(flag) {
 			String temp[] = new String[9];
@@ -107,25 +380,66 @@ public class Main {
 			}
 		}
 	}
-	private static void turnD (Boolean flag) {
-		
-	}
-	private static void turnF (Boolean flag) {
-		
-	}
-	private static void turnB (Boolean flag) {
-	
-	}
-	private static void turnL (Boolean flag) {
-		
-	}
 	private static void turnR (Boolean flag) {
-	
-	}
-	
-	private static void swap (String a, String b) {
-		String temp = b;
-		b = a;
-		a = temp;
+		// '+' 시계방향
+		if(flag) {
+			String temp[] = new String[9];
+			//cube[0][0][2] -> cube[3][0][2]
+			//cube[0][1][2] -> cube[3][1][2]
+			//cube[0][2][2] -> cube[3][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[3][i][2];
+				cube[3][i][2] = cube[0][i][2];
+			}
+			//cube[3][0][2] -> cube[1][0][2]
+			//cube[3][1][2] -> cube[1][1][2]
+			//cube[3][2][2] -> cube[1][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i+3]=cube[1][i][2];
+				cube[1][i][2] = temp[i];
+			}
+			//cube[1][0][2] -> cube[2][0][2]
+			//cube[1][1][2] -> cube[2][1][2]
+			//cube[1][2][2] -> cube[2][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6]=cube[2][i][2];
+				cube[2][i][2] = temp[i+3];
+			}
+			//cube[2][0][2] -> cube[0][0][2]
+			//cube[2][1][2] -> cube[0][1][2]
+			//cube[2][2][2] -> cube[0][2][2]
+			for(int i=0; i<3; i++) {
+				cube[0][i][2] = temp[i+6];
+			}
+		} else {
+			String temp[] = new String[9];
+			//cube[0][0][2] -> cube[2][0][2]
+			//cube[0][1][2] -> cube[2][1][2]
+			//cube[0][2][2] -> cube[2][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i] = cube[3][i][2];
+				cube[2][i][2] = cube[0][i][2];
+			}
+			//cube[2][0][2] -> cube[1][0][2]
+			//cube[2][1][2] -> cube[1][1][2]
+			//cube[2][2][2] -> cube[1][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i+3]=cube[1][i][2];
+				cube[1][i][2] = temp[i];
+			}
+			//cube[1][0][2] -> cube[3][0][2]
+			//cube[1][1][2] -> cube[3][1][2]
+			//cube[1][2][2] -> cube[3][2][2]
+			for(int i=0; i<3; i++) {
+				temp[i+6]=cube[3][i][2];
+				cube[3][i][2] = temp[i+3];
+			}
+			//cube[3][0][2] -> cube[0][0][2]
+			//cube[3][1][2] -> cube[0][1][2]
+			//cube[3][2][2] -> cube[0][2][2]
+			for(int i=0; i<3; i++) {
+				cube[0][i][2] = temp[i+6];
+			}
+		}
 	}
 }
--- algorithm/Cubing/src/test/java/MainTest.java
+++ algorithm/Cubing/src/test/java/MainTest.java
@@ -7,8 +7,8 @@ public class MainTest {
 	@Test
 	public void test() {
 		//int cases[][] = {}; 
-		String[][] res = {{"L+", "owwbwwbww"}, {"L-", "rwwrwwrww"}};
-		for(int i=0; i<3; i++)
+		String[][] res = {{"L+", "owwowwoww"}, {"L-", "rwwrwwrww"},{"F+ B+", "bbbwwwggg"} , {"L- U- L+ U- L- U- U- L+ U+ U+", "gwowwwrww"}};
+		for(int i=0; i<res.length; i++)
 			assertEquals(res[i][1], Main.solve1(res[i][0]));
 	}
 
